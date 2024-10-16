33142fa45b9e4cba9fe61b8151254356a73df36b
GaturaN <gaturanjenga@gmail.com>
2024-10-16 20:57:06 +0300

    feat: toggle fields in assist

--- assist/assist/doctype/assist/assist.js
+++ assist/assist/doctype/assist/assist.js
@@ -1,8 +1,80 @@
 // Copyright (c) 2024, Gatura Njenga and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Assist', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Assist", {
+  refresh: function (frm) {
+    // Call the function to toggle necessary fields on refresh
+    toggle_necessary_fields(frm);
+  },
 
-	// }
+  involves_customer: function (frm) {
+    // Call the function when 'involves_customer' is changed
+    toggle_necessary_fields(frm);
+  },
+
+  involves_supplier: function (frm) {
+    // Call the function when 'involves_supplier' is changed
+    toggle_necessary_fields(frm);
+  },
+
+  involves_item: function (frm) {
+    // Call the function when 'involves_item' is changed
+    toggle_necessary_fields(frm);
+  },
+
+  involves_payment: function (frm) {
+    // Call the function when 'involves_payment' is changed
+    toggle_necessary_fields(frm);
+  },
+
+  before_save: function (frm) {
+    // Check if the form is new and set the 'raised_by' field to the current user
+    if (frm.is_new()) {
+      frm.set_value("raised_by", frappe.session.user);
+    }
+  },
 });
+
+// toggle_necessary_fields function
+function toggle_necessary_fields(frm) {
+  // Set field values from the form
+  let involves_customer = frm.doc.involves_customer;
+  let involves_supplier = frm.doc.involves_supplier;
+  let involves_item = frm.doc.involves_item;
+  let involves_payment = frm.doc.involves_payment;
+
+  //  toggle lists
+  let customer_fields = ["customer", "sales_order", "sales_invoice"];
+  let supplier_fields = [
+    "supplier",
+    "purchase_order",
+    "purchase_receipt",
+    "purchase_invoice",
+  ];
+
+  //   toggle customer fields
+  if (involves_customer) {
+    frm.toggle_display(customer_fields, true);
+    console.log("working!!");
+  } else {
+    frm.toggle_display(customer_fields, false);
+  }
+
+  if (involves_supplier) {
+    frm.toggle_display(supplier_fields, true);
+  } else {
+    frm.toggle_display(supplier_fields, false);
+  }
+
+  if (involves_item) {
+    frm.toggle_display("item", true);
+  } else {
+    frm.toggle_display("item", false);
+  }
+
+  if (involves_payment) {
+    frm.toggle_display("payment_entry", true);
+  } else {
+    frm.toggle_display("payment_entry", false);
+  }
+}
