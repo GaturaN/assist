c6a11678f7cee649c354059b11a68237560f97ac
GaturaN <gaturanjenga@gmail.com>
2024-11-05 10:15:18 +0300

    feat: listview filter

---
+++ assist/assist/doctype/assist/assist_list.js
@@ -0,0 +1,24 @@
+// looged in user should only view documents raised_by, assigned_to or escalated_to them.
+frappe.listview_settings["Assist"] = {
+  onload(listview) {
+    // Check if the user has the System Manager role
+    const isSystemManager = frappe.user_roles.includes("System Manager");
+
+    // Apply filters if the user is not a System Manager
+    if (!isSystemManager) {
+      listview.filter_area.add(
+        [
+          ["Assist", "raised_by", "=", frappe.session.user],
+          ["Assist", "assigned_to", "=", frappe.session.user],
+          ["Assist", "escalated_to", "=", frappe.session.user],
+        ],
+        "OR"
+      ); // Add as an OR condition for any one of the filters to match
+    }
+
+    // Set interval to refresh the list every 10 seconds
+    setInterval(() => {
+      listview.refresh();
+    }, 10000);
+  },
+};
